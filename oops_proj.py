8fad048c32d978de13d81f14d9eeb4ff1768927c
Abhay <tabhay373@gmail.com>
2025-04-14 16:22:12 +0530

    project completed

--- oops_proj.py
+++ oops_proj.py
@@ -11,15 +11,16 @@ class chatbook:
                                2. press 2 to log in 
                                3. press 3 to write a post
                                4. press 4 to message a friend
-                               5. press any other key to exit""")
+                               5. press any other key to exit
+                                    -->   """)
         if user_input == "1":
             self.signup()
         elif user_input == "2":
             self.loggin()
         elif user_input == "3":
-            pass 
+            self.post()
         elif user_input == "4":
-            pass 
+            self.msg() 
         else:
             exit()
 
@@ -29,6 +30,7 @@ class chatbook:
         pw = input("enter a valid password  --> ")
         self.username = email 
         self.password = pw
+        print("\n")
         print("you have succesfully signed up !!")
         print("\n")
         self.menu()
@@ -40,14 +42,35 @@ class chatbook:
         else:
             uname = input("enter your email/username : ")
             p = input("enter your password : ")
+            print("\n")
             if self.username == uname and self.password == p:
                 print("you have successfully logged in !!")
                 self.loggedin = True 
             else:
-                print("you have entered incorrect password , pleas enter correct username/password")
+                print("you have entered incorrect password ")
+                print("please enter correct username/password or sign up first")
             print("\n")
             self.menu()
 
+    def post(self):
+        if self.loggedin == True:
+            ps = input("write a post here : ")
+            print("you have posted : " + ps)
+        else:
+            print("please logg in first by clicking 2")
+        print("\n")
+        self.menu()
+
+    def msg(self):
+        if self.loggedin == True:
+            ms = input("enter your msg : ")
+            fr = input("to whom you want to send this msg : ")
+            print(f"msg sent to {fr}  successfully !!")
+        else:
+            print("please logg in first by clicking 2 ")
+        print("\n")
+        self.menu()
 
 
-obj = chatbook()
+
+user1 = chatbook()
